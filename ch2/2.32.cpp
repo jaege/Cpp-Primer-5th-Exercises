761a0db0bf4b72d78b1cb0aa4e139c3a9421495d
Frank Wang <frankwang.trading@gmail.com>
2019-09-07 16:19:40 -0500

    deleted method 1 & 5
    Neither of them is a valid solution.

--- ch2/2.32.cpp
+++ ch2/2.32.cpp
@@ -3,10 +3,6 @@
 int main() {
   //int null = 0, *p = null;  // Error: type mismatch
 
-  // Method 1
-  const int null1 = 0;
-  int *p1 = null1;
-
   // Method 2
   int *p2 = nullptr;
 
@@ -15,10 +11,6 @@ int main() {
 
   // Method 4 (may need include `cstdlib`)
   int *p4 = NULL;
-
-  // Method 5
-  constexpr int null2 = 0;
-  int *p5 = null2;
 
   return 0;
 }
