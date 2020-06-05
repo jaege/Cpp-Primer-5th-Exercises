bfd2e6741c556b77a088c766e0d6f4d183c2b91e
Walter <jaege@users.noreply.github.com>
2020-06-04 23:18:59 -0700

    Update 5.14.cpp

--- ch2/2.32.cpp
+++ ch2/2.32.cpp
@@ -3,9 +3,12 @@
 int main() {
   //int null = 0, *p = null;  // Error: type mismatch
 
+  // Method 0 (by MIC)
+  int null = 0, *p = &null;
+
   // Method 1
   const int null1 = 0;
-  int *p1 = null1;
+  // int *p1 = null1; err
 
   // Method 2
   int *p2 = nullptr;
@@ -18,7 +21,7 @@ int main() {
 
   // Method 5
   constexpr int null2 = 0;
-  int *p5 = null2;
+  // int *p5 = null2; err
 
   return 0;
 }
