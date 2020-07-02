88feed2894c0bacbd6518fdfeccaae423bd3a742
Eason51 <66631204+Eason51@users.noreply.github.com>
2020-07-02 20:08:37 +0800

    Update 10.37.cpp

--- ch10/10.37.cpp
+++ ch10/10.37.cpp
@@ -4,7 +4,7 @@
 
 int main() {
   std::vector<int> vi{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  std::list<int> li(vi.rbegin() + 2, vi.rend() - 3);
+  std::list<int> li(vi.rbegin() + 3, vi.rend() - 2);
   for (const auto &i : li)
     std::cout << i << " ";
 
